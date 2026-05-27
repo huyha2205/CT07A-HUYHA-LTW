14ad6437e140dcc0aa4312e60e0be902912d16f4
huyha2205 <151826116+huyha2205@users.noreply.github.com>
2026-05-27 10:07:45 +0000

    dong bo

--- dhv_web_java1/src/main/webapp/index.js
+++ dhv_web_java1/src/main/webapp/index.js
@@ -1,5 +1,44 @@
 const form= document.getElementById("register-form")
 const username=document.getAnimations("username")
 const email=document.getAnimations("email")
-const password=document.getAnimations("password"
+const password=document.getAnimations("password")
+const confirmPassword=document.getAnimations("confirmPassword")
+
+form.addEventListener("submit",function(e){
+    //e=event
+    e.preventDefault();
+    const isRequiredValid=checkRequired([username,email,password,confirmPassword])
+});
+
+function checkRequired(inputArray){
+    let isValid= true;
+    inputArray.forEach((input)=>{
+        if(input.value.trim()==="")
+        {
+            showError(input,'${formatFieldName(inout)}is required');
+            isValid=false;
+        }else{
+            showSuccess(input);
+        }
+    });
+    return isValid;
+}
+
+function formatFieldName(input){
+    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+}
+
+function showError(input,message){
+    const formGroup=input.parentElement;
+    formGroup.className="form-group error";
+    const small = formGroup.querySelector("small");
+    small.innerText=message;
+}
+
+function showSuccess(input){
+    const formGroup=input.parentElement;
+    formGroup.className="form_group success";
+}
+
+
 
